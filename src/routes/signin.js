c8df7062e52516c534f3cc026ef648e1b0253502
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-10-11 00:43:41 +0000

    edit the ups and fedex creating endpoints

--- src/routes/signin.js
+++ src/routes/signin.js
@@ -3,10 +3,9 @@
 require('dotenv').config();
 
 const routers = require('express').Router();
-const { usermodel } = require('../models/index')
 const basic = require('../middlewares/basic')
 
 routers.post('/signin/users', basic, async (req, res) => {
-res.status(200).send(req.User)
+    res.status(200).send(req.User)
 })
 module.exports = routers
