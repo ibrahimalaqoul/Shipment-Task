933ccad3770823f2d562e965cc0333cc23ce501b
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-10-11 12:07:18 +0000

    adding the cors middleware for who can access the server

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ const relationalRoutes = require('./routes/relationalRoutes')
 
 //middlewares
 app.use(express.json());// body parsing
+app.use(cors()) // making our server as a public server
 app.use(signUpRouter);
 app.use(signinRouter);
 app.use(fedexRouters);
